df535f9b0a3efc16641eb980447e6a10bcbc7d13
Lucas Busso <lucasxbusso@gmail.com>
2022-11-28 23:41:14 -0300

    basic styles added

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -43,27 +43,33 @@ const TaskForm = () => {
         if(params.id){
             setTask(tasks.find(task => task.id === params.id));
         }
-    }, []);
+    }, [params.id, tasks]);
 
     return (
-        <form 
-            onSubmit={handleSubmit}
-        >
-        <input 
-            name="title"
-            type="text" 
-            placeholder="Task name" 
-            onChange={handleChange} 
-            value={task.title}
-            />
+      <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-10">
+        <label htmlFor="title" className="block text-sm font-bold mb-2">
+          Task:
+        </label>
+        <input
+          name="title"
+          type="text"
+          placeholder="Task name"
+          onChange={handleChange}
+          value={task.title}
+          className="w-full p-2 rounded-md bg-zinc-600 mb-2"
+        />
+        <label htmlFor="description" className="block text-sm font-bold mb-2">
+          Description
+        </label>
         <textarea
-            name="description"
-            placeholder="Task description"
-            onChange={handleChange}
-            value={task.description}
+          name="description"
+          placeholder="Task description"
+          onChange={handleChange}
+          value={task.description}
+          className="w-full p-2 rounded-md bg-zinc-600 mb-2"
         ></textarea>
-        <button type="submit">Save</button>
-        </form>
+        <button type="submit" className='bg-indigo-600 px-2 py-1 w-full rounded mt-5'>Save</button>
+      </form>
     );
 }
 
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -11,19 +11,39 @@ const TaskList = () => {
   }
 
   return (
-    <div>
+    <div className="w-4/6">
+      <header className="flex justify-between py-4 items-center">
         <h1>{stateTask.length} tasks</h1>
-        <Link to="/create-task">Create task</Link>
-        {stateTask.map(task => (
-            <div key={task.id}>
-                <h3>{task.title}</h3>
-                <p>{task.description}</p>
-                <button onClick={() => handleDelete(task.id)}>Delete</button>
-                <Link to={`/edit-task/${task.id}`}>Edit task</Link>
-            </div>
-        ))}        
+        <Link to="/create-task" className="bg-indigo-600 px-2 py-1">
+          Create task
+        </Link>
+      </header>
+      <div className="grid grid-cols-3 gap-4">
+        {stateTask.map((task) => (
+          <div key={task.id} className="bg-neutral-800 p-4 rounded-md">
+            <header className="flex justify-between">
+              <h3>{task.title}</h3>
+              <div className='flex gap-x-2'>
+                <Link 
+                    to={`/edit-task/${task.id}`}
+                    className='bg-zinc-600 px-2 py-1 text-xs rounded-md'
+                >
+                    Edit
+                </Link>
+                <button 
+                    onClick={() => handleDelete(task.id)}
+                    className="bg-red-500 px-2 py-1 text-xs rounded-md"
+                >
+                    Delete
+                </button>
+              </div>
+            </header>
+            <p>{task.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default TaskList
